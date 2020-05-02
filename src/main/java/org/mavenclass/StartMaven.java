33ef39528ce2dfa06ca14c515b21b9b4f7480315
Priyobrato Das <priyodas12@gmail.com>
2020-05-02 20:45:16 +0530

    added new maven life cycle method details

--- src/main/java/org/mavenclass/StartMaven.java
+++ src/main/java/org/mavenclass/StartMaven.java
@@ -1,11 +1,19 @@
 package org.mavenclass;
-/*validate - validate the project is correct and all necessary information is available
-compile - compile the source code of the project
-test - test the compiled source code using a suitable unit testing framework. These tests should not require the code be packaged or deployed
+/*
+validate - validate the project is correct and all necessary information is available.
+
+compile - compile the source code of the project.
+
+test - test the compiled source code using a suitable unit testing framework. These tests should not require the code be packaged or deployed.
+
 package - take the compiled code and package it in its distributable format, such as a JAR.
+
 verify - run any checks on results of integration tests to ensure quality criteria are met
+
 install - install the package into the local repository, for use as a dependency in other projects locally
-deploy - done in the build environment, copies the final package to the remote repository for sharing with other developers and projects.*/
+
+deploy - done in the build environment, copies the final package to the remote repository for sharing with other developers and projects.
+*/
 public class StartMaven {
     public static void main(String[] args) {
         System.out.println("Helo Maven...initial project set up..");
