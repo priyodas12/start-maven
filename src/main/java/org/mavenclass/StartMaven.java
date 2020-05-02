37b3dcbfb1b53021b17e27d679fdb1ddf8b28724
Priyobrato Das <priyodas12@gmail.com>
2020-05-02 20:29:02 +0530

    added new sysout message

--- src/main/java/org/mavenclass/StartMaven.java
+++ src/main/java/org/mavenclass/StartMaven.java
@@ -2,6 +2,6 @@ package org.mavenclass;
 
 public class StartMaven {
     public static void main(String[] args) {
-        System.out.println("Helo Maven...initial project set up");
+        System.out.println("Helo Maven...initial project set up..");
     }
 }
